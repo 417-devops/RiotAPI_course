ce068faa518b3c0e998a067963f75f0e5f6b31a1
Karl Roush <karl.roush+github@gmail.com>
2022-02-08 15:01:19 -0500

    Finished video recordings

--- Module 1- Basics/module1_examples.py
+++ Module 1- Basics/module1_examples.py
@@ -3,6 +3,15 @@
 Created on Tue Feb  8 14:12:37 2022
 
 @author: compute
+
+Desc: Riot API Bootcamp Module 1 example
 """
 
-print("hello world")
+import pandas as pd
+import matplotlib.pyplot as plt
+
+air_quality= pd.read_csv('air_quality_no2.csv', index_col=0, parse_dates=True)
+print(air_quality.head())
+
+
+air_quality.plot()
