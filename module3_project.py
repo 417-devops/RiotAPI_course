dc4d60ea4ce28eae53c12b896b2427480b3b2ec5
karlroush <karl.roush+github@gmail.com>
2022-02-28 16:33:17 -0500

    Added solutions

--- module3_project.py
+++ module3_project.py
@@ -3,13 +3,13 @@
 Created on Fri Feb 11 10:15:55 2022
 
 Author: 417-DevOps
-Desc: Riot API Bootcamp Module 3 example (automating account info call)
+Desc: Riot API Bootcamp Module 3 project (automating account info call)
 """
-
 ##--------- LOAD LIBRARIES ---------##
 from riotwatcher import LolWatcher #'pip install riotwatcher' in Anaconda prompt
 from dotenv import load_dotenv #'pip install python-dotenv' in Anaconda prompt
 import os
+
 
 ##--------- LOAD CONFIG DATA ---------##
 # You can replace this with your API key, I just keep mine outside of repo directory
@@ -24,7 +24,9 @@ player_name= 'RebirthNA'
 player_region= 'NA1'.lower() #[BR1, EUN1, EUW1, JP1, KR, LA1, LA2, NA1, OC1, TR1, RU]  
 player_routing= 'americas'
 
+
 ##--------- CREATE PLAYER OBJECT ---------##
 # This is equivalent to going to /riot/account/v1/accounts/by-riot-id/
 summoner= lol_watcher.summoner.by_name(player_region, player_name)
 print('Player info= \n',summoner)
+
--- module4_project.py
+++ module4_project.py
@@ -1,17 +1,15 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Feb 16 18:54:13 2022
+Created on Mon Feb 14 17:50:21 2022
 
 Author: 417-DevOps
-Desc: Riot API Bootcamp Module 5-1 example (memory usage)
+Desc: Riot API Bootcamp Module 4 project (request past games, determine gametime)
 """
 
 #%% ##--------- LOAD LIBRARIES ---------##
 from riotwatcher import LolWatcher #'pip install riotwatcher' in Anaconda prompt
 from dotenv import load_dotenv #'pip install python-dotenv' in Anaconda prompt
 import os
-
-from sys import getsizeof
 
 def setup_env():
     load_dotenv('../../config.env')
@@ -28,25 +26,24 @@ lol_watcher= setup_env()
 
 ##--------- SET PLAYER PARAMETERS ---------##
 player_name= 'RebirthNA'
+num_matches_data= 10
 player_region= 'NA1'.lower() #[BR1, EUN1, EUW1, JP1, KR, LA1, LA2, NA1, OC1, TR1, RU]  
 player_routing= 'americas'
-
 
 ##--------- LOAD THE PLAYER DATA AS CLASS ---------##
 summoner= lol_watcher.summoner.by_name(player_region, player_name)
 match_history= lol_watcher.match.matchlist_by_puuid(region= player_routing, puuid= summoner['puuid'],
                                                     queue= 420, 
-                                                    start=0, count= 1)
-matchID= match_history[0]
+                                                    start=0, count= num_matches_data)
 
-
-match_data_big= lol_watcher.match.by_id(region= player_routing, match_id= matchID)
-game_time= match_data_big['info']['gameDuration']
-print('Basic game duration size [bytes]= ', getsizeof(match_data_big)+getsizeof(game_time))
-
-
-match_data_small= lol_watcher.match.by_id(region= player_routing, match_id= matchID)['info']['gameDuration']
-print('More efficient method size [bytes]= ', getsizeof(match_data_small))
-
-
-print('2nd method is', round(getsizeof(match_data_big)/getsizeof(match_data_small),2), 'times smaller')
+# See example file in folder for match data, info at
+# https://developer.riotgames.com/apis#match-v5/GET_getMatch 
+print('Game times= ')
+for matchID in match_history:
+    # explanation way
+    match_data= lol_watcher.match.by_id(region= player_routing, match_id= matchID)
+    game_time= match_data['info']['gameDuration']
+    
+    # better way to save memory
+    match_data= lol_watcher.match.by_id(region= player_routing, match_id= matchID)['info']['gameDuration']
+    print(game_time)
